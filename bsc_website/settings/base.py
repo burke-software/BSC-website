b331dd28aed415217a4f2dc48b182ce0b0feea5b
David Burke <dmbst32@gmail.com>
2014-09-09 16:51:40 -0400

    fixed s3 settings

--- bsc_website/settings/base.py
+++ bsc_website/settings/base.py
@@ -236,8 +236,6 @@ if os.getenv('USE_S3'):
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_STORAGE_BUCKET_PATH = os.getenv('AWS_STORAGE_BUCKET_PATH', '')
-    COMPRESS_URL = STATIC_URL = 'https://{}.s3.amazonaws.com/{}/'.format(
+    COMPRESS_URL = STATIC_URL = 'https://{}.s3.amazonaws.com/'.format(
             AWS_STORAGE_BUCKET_NAME,
-            AWS_STORAGE_BUCKET_PATH,
         )
