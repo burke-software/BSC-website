f721b8294eebfbf0b97ef268d0de075eab4daa32
michellejaeg <michelles82@gmail.com>
2014-09-18 13:43:18 -0700

    corrected error

--- website/blog_migration/xml_parse.py
+++ website/blog_migration/xml_parse.py
@@ -4,44 +4,40 @@ This script migrates blog posts from WordPress to Wagtail.
 
 from lxml import etree
 from bs4 import BeautifulSoup
-from website.models import
+from website.models import BlogPage, BlogPageTag, Tag, BlogIndexPage
 from django.template.defaultfilters import slugify
 from django.db.utils import IntegrityError
 import re
 
-xml_file = 'gkblog/greenkeyresources.wordpress.2014-06-30.xml'
+xml_file = 'website/blog_migration/technologyagainstyou.wordpress.2014-09-16.xml'
 tree = etree.parse(xml_file)
 root = tree.getroot()
 
 soup = BeautifulSoup(etree.tostring(root), 'xml')
-
-
-blog_categories = ['Accounting/Finance', 'Alternative Asset Management', 'Financial Services', 'General Interest',
-                       'Green Key In the Community', 'Healthcare', 'HR & Management Tips', 'Human Resources',
-                       'Information Technology', 'Job Search Advice', 'Legal', 'New Job Advice', 'Office Support',
-                        'Pharmaceutical', 'Temporary Staffing']
-
-for blog_category in blog_categories:
-    try:
-        BlogCategory.objects.create(name=blog_category, slug=slugify(blog_category))
-    except IntegrityError:
-        pass
-
-path = 0
+counter = 0
 for item in soup.findAll('item'):
     if item.find('encoded').string:
         link = item.find('link').string
-        if re.findall('\d\d\d\d/\d\d/[^/]+/?$', link):
+        if re.findall('\d\d\d\d/\d\d/\d\d/[^/]+/?$', link):
             link = re.findall('/([^/]+)/?$', link)[0]
         else:
             continue
         body = item.find('encoded').string
         body = '<p> ' + body + ' </p>'
         body = body.replace('\n\n', '</p> <p>')
-        #body = body.encode("ascii", "xmlcharrefreplace")
+        body = body.encode("ascii", "xmlcharrefreplace")
         date = item.find('post_date').string[0:10]
         tags = [tag.string.replace('&amp;', '&') for tag in item.findAll('category', {'domain': 'post_tag'})]
         tag_objects = []
+        counter += 1
+        if len(str(counter)) == 1:
+            the_path = '000100010001000{}'.format(str(counter))
+        if len(str(counter)) == 2:
+            the_path = '00010001000100{}'.format(str(counter))
+        if len(str(counter)) == 3:
+            the_path = '0001000100010{}'.format(str(counter))
+        if len(str(counter)) == 4:
+            the_path = '000100010001{}'.format(str(counter))
         # Create wagtail tag objects
         for tag in tags:
             try:
@@ -50,28 +46,23 @@ for item in soup.findAll('item'):
                 a_tag = Tag.objects.get(name=tag)
             tag_objects.append(a_tag)
         title = item.find('title').string
-        #title = title.encode("ascii", "xmlcharrefreplace")
-        categories = [cat.string.replace('&amp;', '&') for cat in item.findAll('category', {'domain': 'category'})]
-        path += 1
-        if len(str(path)) == 1:
-            the_path = '00010001000{}'.format(str(path))
-        if len(str(path)) == 2:
-            the_path = '0001000100{}'.format(str(path))
-        if len(str(path)) == 3:
-            the_path = '000100010{}'.format(str(path))
-        if len(str(path)) == 4:
-            the_path = '00010001{}'.format(str(path))
+        popular_post_dates = ['2012-04-26', '2011-08-10', '2013-08-20', '2013-04-21', '2010-09-16', '2010-01-12',
+                              '2010-10-17']
         try:
-            blog_page = BlogPage.objects.create(body=body, date=date, title=title, depth=3, path=the_path,
+            if date[3] == '3' or date[3] == '4' or date in popular_post_dates:
+                print date
+                blog_page = BlogPage.objects.create(body=body, date=date, title=title, depth=4, path=the_path,
                                                     slug=link)
-            for category in categories:
-                blog_category = BlogCategory.objects.get(name=category)
-                blog_page_category = BlogCategoryBlogPage.objects.create(category=blog_category, page=blog_page)
-            # Creates blog page tag objects
-            for tag in tag_objects:
-                blog_tag = BlogPageTag.objects.create(content_object=blog_page, tag=tag)
-        except:
+
+                # Creates blog page tag objects
+                for tag in tag_objects:
+                    blog_tag = BlogPageTag.objects.create(content_object=blog_page, tag=tag)
+        except IntegrityError:
             pass
+
+
+
+
 
 
 
