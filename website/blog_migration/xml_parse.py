603bdbd5fd3a2b710a4eab7e98e82ee1c6116a4e
michellejaeg <michelles82@gmail.com>
2014-09-22 16:08:46 -0700

    added dealing with images to blog migration script

--- website/blog_migration/xml_parse.py
+++ website/blog_migration/xml_parse.py
@@ -8,36 +8,52 @@ from website.models import BlogPage, BlogPageTag, Tag, BlogIndexPage
 from django.template.defaultfilters import slugify
 from django.db.utils import IntegrityError
 import re
+import urllib
+from bsc_website import settings
+import os
+
 
 xml_file = 'website/blog_migration/technologyagainstyou.wordpress.2014-09-16.xml'
+path_of_blog_index = '000100010003'
+
 tree = etree.parse(xml_file)
 root = tree.getroot()
 
 soup = BeautifulSoup(etree.tostring(root), 'xml')
 counter = 0
+keepers = 0
 for item in soup.findAll('item'):
     if item.find('encoded').string:
+        body = item.find('encoded').string
+        body = '<p> ' + body + ' </p>'
+        body = body.replace('\n\n', '</p> <p>')
+        body = body.encode("ascii", "xmlcharrefreplace")
+
+        for image_tag in re.findall("(<img\s[^>]*?src\s*=\s*['\"]([^'\"]*?)['\"][^>]*?>)", body):
+            image_tag_text = image_tag[0]
+            image_tag_src = image_tag[1]
+            filename = re.findall("/([^/]*)$", image_tag_src)[0]
+            urllib.urlretrieve(image_tag_src, os.path.join(settings.MEDIA_ROOT, "images", filename))
+            new_url = settings.MEDIA_URL + 'images/' + filename
+            body = body.replace(image_tag_src, new_url)
+
         link = item.find('link').string
         if re.findall('\d\d\d\d/\d\d/\d\d/[^/]+/?$', link):
             link = re.findall('/([^/]+)/?$', link)[0]
         else:
             continue
-        body = item.find('encoded').string
-        body = '<p> ' + body + ' </p>'
-        body = body.replace('\n\n', '</p> <p>')
-        body = body.encode("ascii", "xmlcharrefreplace")
         date = item.find('post_date').string[0:10]
         tags = [tag.string.replace('&amp;', '&') for tag in item.findAll('category', {'domain': 'post_tag'})]
         tag_objects = []
         counter += 1
         if len(str(counter)) == 1:
-            the_path = '000100010003000{}'.format(str(counter))
+            the_path = path_of_blog_index + '000{}'.format(str(counter))
         if len(str(counter)) == 2:
-            the_path = '00010001000300{}'.format(str(counter))
+            the_path = path_of_blog_index + '00{}'.format(str(counter))
         if len(str(counter)) == 3:
-            the_path = '0001000100030{}'.format(str(counter))
+            the_path = path_of_blog_index + '0{}'.format(str(counter))
         if len(str(counter)) == 4:
-            the_path = '000100010003{}'.format(str(counter))
+            the_path = path_of_blog_index + '{}'.format(str(counter))
         # Create wagtail tag objects
         for tag in tags:
             try:
@@ -50,15 +66,32 @@ for item in soup.findAll('item'):
                               '2010-10-17']
         try:
             if date[3] == '3' or date[3] == '4' or date in popular_post_dates:
-                print date
                 blog_page = BlogPage.objects.create(body=body, date=date, title=title, depth=4, path=the_path,
                                                     slug=link)
+                keepers += 1
 
                 # Creates blog page tag objects
                 for tag in tag_objects:
                     blog_tag = BlogPageTag.objects.create(content_object=blog_page, tag=tag)
         except IntegrityError:
             pass
+# Set numchild for BlogIndexPage in database
+blog_index_page = BlogIndexPage.objects.filter(path=path_of_blog_index)[0]
+blog_index_page.numchild = keepers
+blog_index_page.save()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
