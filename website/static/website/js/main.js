1ac5927a5d6d4d1943746962410fd806ea650e8c
Brendan <brendan@burkesoftware.com>
2014-10-07 17:24:38 -0400

    Make 'Meet the team' page mobile-friendly

--- website/static/website/js/main.js
+++ website/static/website/js/main.js
@@ -6,4 +6,35 @@ $(document).ready(function(){
 
     //Apply img-thumbnail class to body-content images
     $('.body-content img').addClass("img-thumbnail");
+
+    // For the Person page. Hide bios at smaller sizes.
+    showOrHideBio();
+});
+
+
+function showOrHideBio() {
+	if ($(window).width() < 767) {
+	    $('.person-biography').hide();
+	} else {
+	    $('.person-biography').show();
+	}
+};
+
+var resizeTimer;
+$(window).resize(function() {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(showOrHideBio, 100);
+});
+
+
+$('.person-bio-trigger').click(function() {
+	if ($(window).width() < 767) {
+		$(this).parent().next('.person-biography').slideToggle();
+
+		if ($(this).find('.fa').hasClass('fa-angle-right')) {
+			$(this).find('.fa').removeClass('fa-angle-right').addClass('fa-angle-down');
+		} else if ($(this).find('.fa').hasClass('fa-angle-down')) {
+			$(this).find('.fa').removeClass('fa-angle-down').addClass('fa-angle-right');
+		}
+	}
 });
